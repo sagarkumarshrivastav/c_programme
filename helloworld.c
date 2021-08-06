d4d5667b21c00194545bc9d7f70f4653d20848f9
sagarkumarshrivastav <87256166+sagarkumarshrivastav@users.noreply.github.com>
2021-08-06 11:50:43 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 #include<conio.h>
 
-int main()
-{
-clrscr();
-printf("Hello World");
-return 0;
-}
+// void main() ---> void is use for null value this means after the execution of given code it returns null value.
+   int main()
+ {
+   clrscr();
+     printf("Hello World");
+   return 0;
+// getch (); ---> getch() instruction is a function of stdio.h library which is use to get a character.
+ }
